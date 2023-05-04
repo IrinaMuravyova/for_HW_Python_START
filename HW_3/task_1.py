cc1fb101d75e3bb6791dd86b528320508c4dd633
Irina Muravyeva <irinamuravyeva@MacBook-Pro-Irina.local>
2023-05-04 15:13:29 +0500

    the task #1 from HW #3  was completed

---
+++ HW_3/task_1.py
@@ -0,0 +1,22 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai (можно использовать псевдогенерацию). 
+# Последняя строка содержит число X.
+# *Пример:*
+# 5
+# 7 -2 3 5 1
+# 3
+# -> 1
+
+import random
+
+countEl = int(input('Input count of elements: '))
+# if countEl > 0:
+lst = [random.randint(1, countEl) for i in range(countEl)]
+# else: print('Enter a whole natural number')
+print(lst)
+findEl = int(input('Input number for search: '))
+for el in lst:
+    if el == findEl:
+        print(lst.count(el))
+        break
